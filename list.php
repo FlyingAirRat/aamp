c4d2d0d38ae864cbd4bb2f665b0795de1a36fd64
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-05-16 17:04:29 +0900

    list.css 추가

--- list.php
+++ list.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    include_once "header.php";
     include_once "db/db_img.php";
     $list = sel_list();
 ?>
@@ -9,10 +11,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stlyesheet" href="./list.css">
     <title>목록</title>
 </head>
 <body>
-    <div>
+    <div class="container">
         <?php
             foreach($list as $item){
         ?>
