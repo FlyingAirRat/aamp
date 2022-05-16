e4cc2bb84aa24ea375601e27d85431ae8a136620
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-05-16 12:53:08 +0900

    join 수정 / join_proc 생성

--- db/db_user.php
+++ db/db_user.php
@@ -1,12 +1,31 @@
 <?php
   include_once "db.php";
 
+  function ins_user(&$param){
+    $u_lv = $param['u_lv'];
+    $uid = $param['uid'];
+    $upw = $param['upw'];
+    $nm = $param['nm'];
+
+    $sql =
+    " INSERT INTO info_user
+      (u_lv, uid, upw, user_nm)
+      VALUES
+      ('$u_lv', '$uid', '$upw', '$nm')
+    ";
+    $conn = get_conn();
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+
+    return $result;
+  }
+
   function sel_user(&$param){
     $uid = $param['uid'];
     $sql = 
     " SELECT u_no, u_lv, uid, upw, user_nm
       FROM info_user
-      WHERE uid = $uid";
+      WHERE uid = '$uid'";
 
     $conn = get_conn();
     $result = mysqli_query($conn, $sql);
