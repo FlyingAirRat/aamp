1d4b2d3e73e76649fe2ad11c167c001c60875ef9
junirii <105265611+junirii@users.noreply.github.com>
2022-05-17 15:06:14 +0900

    a 수정
    ㅇㅇ

--- class_detail.php
+++ class_detail.php
@@ -51,7 +51,7 @@
                     <td>$start_time ~ $end_time</td>
                     <td>
                       출석한 인원/$people
-                      <a href='' onclick='subform()'>
+                      <a href='' onclick='subform()'>></a>
                     </td>
                   </tr>
                 </form>
