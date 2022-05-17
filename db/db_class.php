1fc1390f0c0294cef70821a5cb3ab3866e54566e
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-05-17 17:48:51 +0900

    쿼리 수정

--- db/db_class.php
+++ db/db_class.php
@@ -42,13 +42,15 @@
   }
 
   function sel_att_img(&$param) {
+
+
     $sql =
     " SELECT A.imgsrc, B.user_nm, A.uploaded_time
       FROM stu_img A
         INNER JOIN info_user B
               ON A.u_no = B.u_no
-      WHERE class_no = {$param['class_no']} 
-        AND att_no = {$param['att_no']}
+      WHERE A.class_no = {$param['class_no']} 
+        AND A.att_no = {$param['att_no']}
     ";
     $conn = get_conn();
     $result = mysqli_query($conn, $sql);
