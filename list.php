04838d3125d1faec5e721d82fc462c79c137502d
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-05-18 12:29:15 +0900

    Update list.php

--- list.php
+++ list.php
@@ -46,7 +46,7 @@
                 let attImg = document.querySelectorAll('.attImg');
                 let uploadTime = document.querySelectorAll('.uploadTime');
                 let i = <?php echo $i ?>;
-                attImg.innerHtml = '<img src="./img/profile.png">';
+                attImg.innerHTML = '<img src="./img/profile.png">';
 
 
             </script>
