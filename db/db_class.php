d98db1f3b25048143a866ca44f1fde3384c598f9
junirii <105265611+junirii@users.noreply.github.com>
2022-05-18 16:40:31 +0900

    Update db_class.php

--- db/db_class.php
+++ db/db_class.php
@@ -1,22 +1,6 @@
 <?php
   include_once "db.php";
 
-  function ins_class(&$param){
-    $class_nm = $param['class_nm'];
-    $people = $param['people'];
-    $u_no = $param['u_no'];
-    $sql = 
-    " INSERT INTO class
-      (class_nm, u_no, people)
-      VALUES
-      ('$class_nm', $u_no, $people)
-    ";
-    $conn = get_conn();
-    mysqli_query($conn, $sql);
-    mysqli_close($conn);
-
-  }
-
   function sel_all_class(){
     $sql = 
     " SELECT class_no, class_nm
@@ -54,22 +38,12 @@
     return $result;
   }
 
-  function sel_class_set(&$param){
-    $class_no = $param['class_no'];
-    $sql = 
-    " SELECT A.class_nm, B.user_nm
-      FROM class A
-      INNER JOIN info_user B
-      ON A.u_no = B.u_no
-      WHERE A.class_no = $class_no";
-  }
-
   function sel_stu_list(&$param) {
     $sql =
-    " SELECT user_nm
+    " SELECT user_nm, u_no
       FROM info_user
       WHERE class_no = {$param['class_no']}
-      ORDER BY user_nm ASC
+      ORDER BY user_nm
     ";
 
     $conn = get_conn();
@@ -79,18 +53,25 @@
   }
 
   function sel_att_img(&$param) {
-
-
     $sql =
-    " SELECT A.imgsrc, B.user_nm, A.uploaded_time
-      FROM stu_img A
-        INNER JOIN info_user B
-              ON A.u_no = B.u_no
-      WHERE A.class_no = {$param['class_no']} 
-        AND A.att_no = {$param['att_no']}
+    " SELECT imgsrc, uploaded_time
+      FROM stu_img 
+      WHERE class_no = {$param['class_no']} 
+        AND att_no = {$param['att_no']}
+        AND u_no = {$param['u_no']}
     ";
     $conn = get_conn();
     $result = mysqli_query($conn, $sql);
     mysqli_close($conn);
     return mysqli_fetch_assoc($result);
+  }
+
+  function sel_class_set(&$param){
+    $class_no = $param['class_no'];
+    $sql = 
+    " SELECT A.class_nm, B.user_nm
+      FROM class A
+      INNER JOIN info_user B
+      ON A.u_no = B.u_no
+      WHERE A.class_no = $class_no";
   }
