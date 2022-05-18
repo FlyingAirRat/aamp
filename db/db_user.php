12227f83f8f01350fe3e08455873e7a6b8c36bef
junirii <105265611+junirii@users.noreply.github.com>
2022-05-18 11:50:24 +0900

    is null
    dd

--- db/db_user.php
+++ db/db_user.php
@@ -39,7 +39,7 @@
     " SELECT u_no, user_nm
       FROM info_user
       WHERE u_lv = 2
-      AND class_no = null
+      AND class_no IS NULL
       ORDER BY user_nm
     ";
     $conn = get_conn();
