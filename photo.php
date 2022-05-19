45f70bbf0071db864d62b328399a949417b0f9ac
junirii <105265611+junirii@users.noreply.github.com>
2022-05-19 17:48:16 +0900

    Update photo.php

--- photo.php
+++ photo.php
@@ -2,7 +2,7 @@
   include_once "./header.php";
   include_once "db/db_class.php";
   if($_COOKIE['uid']){
-      $_SESSION['login_user'] = $_COOKIE['uid'];
+      $_SESSION['uid'] = $_COOKIE['uid'];
   }
   if(isset($_SESSION['login_user'])){
     $login_user = $_SESSION['login_user'];
