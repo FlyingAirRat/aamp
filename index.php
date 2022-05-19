e465432600ca49ccf4f8420494dc980be6089916
junirii <105265611+junirii@users.noreply.github.com>
2022-05-19 18:02:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
+    function post($url, $fields)
+
+    {
+    
+        $post_field_string = http_build_query($fields, '', '&');
+    
+        $ch = curl_init();                                                            // curl 초기화
+    
+        curl_setopt($ch, CURLOPT_URL, $url);                                 // url 지정하기
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);              // 요청결과를 문자열로 반환
+    
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);               // connection timeout : 10초
+    
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);                 // 원격 서버의 인증서가 유효한지 검사 여부
+    
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_field_string);      // POST DATA
+    
+        curl_setopt($ch, CURLOPT_POST, true);                               // POST 전송 여부
+    
+        $response = curl_exec($ch);
+    
+        curl_close ($ch);
+    
+        return $response;
+    
+    }
+
     if(isset($_COOKIE['uid']) && isset($_COOKIE['upw'])){
         post('./login_proc.php', $_COOKIE['uid']);
         post('./login_proc.php', $_COOKIE['upw']);
