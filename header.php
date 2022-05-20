3fc77881893c6f3e06ee74038bad18f31d14c9f0
junirii <105265611+junirii@users.noreply.github.com>
2022-05-20 17:18:45 +0900

    css

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 <link rel="stylesheet" href="./header.css">
 <header>
   <div id="log">
-    <?=$user_nm?>(<?=$lv_nm?>), 안녕하세요.
-    <a href="logout.php" tabindex="-1"><button>로그아웃</button></a>
+    <span><?=$user_nm?>(<?=$lv_nm?>), 안녕하세요.
+    <a href="logout.php" tabindex="-1"><button>로그아웃</button></a><span>
   </div>
 </header>
