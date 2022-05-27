6b829662d59dac94473de170212d9d5752c7a31f
AirRat <airedmc@gmail.com>
2022-05-27 17:33:55 +0900

    refactor code

--- add_time_proc.php
+++ add_time_proc.php
@@ -1,13 +1,13 @@
 <?php
-  include_once "./db/db_class.php";
-  $start_time = $_POST['start_time'];
-  $end_time = $_POST['end_time'];
-  $class_no = $_POST['class_no'];
+include_once "./db/db_class.php";
+$start_time = $_POST['start_time'];
+$end_time = $_POST['end_time'];
+$class_no = $_POST['class_no'];
 
-  $param = [
+$param = [
     'start_time' => $start_time,
     'end_time' => $end_time,
-    'class_no' => $class_no 
-  ];
-  ins_time($param);
-  header("Location: class_detail.php?class_no=$class_no");
+    'class_no' => $class_no
+];
+ins_time($param);
+header("Location: class_detail.php?class_no=$class_no");
