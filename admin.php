3526cd8e9fc56317895eea482e3b5d0fb4f72120
AirRat <airedmc@gmail.com>
2022-07-04 11:52:19 +0900

    admin.php 부트스트랩 적용

--- admin.php
+++ admin.php
@@ -5,6 +5,7 @@ include_once "header.php";
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="admin.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +13,36 @@ include_once "header.php";
 </head>
 
 <body>
-    
+
+<div id="page-wrapper">
+  <!-- 사이드바 -->
+  <div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+      <li class="sidebar-brand">
+        <a href="#">Zeta</a>
+      </li>
+      <li><a href="#">메뉴 1</a></li>
+      <li><a href="#">메뉴 2</a></li>
+      <li><a href="#">메뉴 3</a></li>
+      <li><a href="#">메뉴 4</a></li>
+      <li><a href="#">메뉴 5</a></li>
+      <li><a href="#">메뉴 6</a></li>
+      <li><a href="#">메뉴 7</a></li>
+      <li><a href="#">메뉴 8</a></li>
+      <li><a href="#">메뉴 9</a></li>
+    </ul>
+  </div>
+  <!-- /사이드바 -->
+
+  <!-- 본문 -->
+  <div id="page-content-wrapper">
+    <div class="container-fluid">
+      <h1>간단한 사이드바</h1>
+      <p>메뉴가 많아서 한 페이지를 넘으면 세로 스크롤바 생김</p>
+    </div>
+  </div>
+  <!-- /본문 -->
+</div>
 </body>
 
 </html>
